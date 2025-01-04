89b483c2aff90d688e70fda01d099b1f7b771cab
Kenneth Cula <kenncul123@gmail.com>
2025-01-04 15:36:07 -0500

    part 1 start

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,59 +1,27 @@
+import { useState } from "react"
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const Display = props => <div>{props.value}</div>
+
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  const [value, setValue] = useState(10)
+
+  const setToValue = (newValue) => () => {
+    console.log('value now', newValue)  // print the new value to console
+    setValue(newValue)
   }
 
-  const Header = (props) => {
-    return (
-      <h1>{props.course.name}</h1>
-    )
-  }
-
-  const Part = (props) => {
-    return (
-      <p>{props.part.name} {props.part.exercises}</p>
-    )
-  }
-
-  const Content = (props) => {
-    return (
-      <div>
-        <Part part={props.course.parts[0]}/>
-        <Part part={props.course.parts[1]}/>
-        <Part part={props.course.parts[2]}/>
-      </div>
-    )
-  } 
-  const Total = (props) => {
-    return (
-      <p>
-        Number of exercises {
-          props.course.parts[0].exercises + 
-          props.course.parts[1].exercises + 
-          props.course.parts[2].exercises
-          }
-      </p>
-    )
-  }
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
+      <Display value = {value}/>
+      <Button handleClick={setToValue(1000)} text={'thousand'}/>
+      <Button handleClick={setToValue(0)} text={'reset'}/>
+      <Button handleClick={setToValue(value+1)} text={'increment'}/>
     </div>
   )
 }
