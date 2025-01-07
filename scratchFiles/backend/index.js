c01bc7da77871cecf031461bc6021462228c9520
Kenneth Cula <kenncul123@gmail.com>
2025-01-06 21:42:16 -0500

    frontend and backend complete

--- scratchFiles/backend/index.js
+++ scratchFiles/backend/index.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const cors = require('cors')
+
 const app = express()
+
 app.use(express.json())
+app.use(express.static('dist'))
+app.use(cors())
+
 
 let notes = [
   {
@@ -72,7 +78,7 @@ app.delete('/api/notes/:id', (request, response) => {
     response.status(204).end()
   })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`)
 })
